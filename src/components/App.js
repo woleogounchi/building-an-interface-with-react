98761218474bfc6392d95001636ed8e2b6a151f8
Kolawole Ogounchi <kolawoleogounchi@outlook.com>
2020-05-19 23:03:09 -0400

    Last update
    Code base corresponds to course code up until 04_05e

--- src/components/App.js
+++ src/components/App.js
@@ -15,16 +15,26 @@ class App extends Component {
       formDisplay: false,
       orderBy: 'petName',
       orderDir: 'asc',
+      queryText: 'ba',
       lastIndex: 0
     };
     this.deleteAppointment = this.deleteAppointment.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
     this.addAppointment = this.addAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
+    this.searchApts = this.searchApts.bind(this);
   }
 
   toggleForm() {
     this.setState({
       formDisplay: !this.state.formDisplay
+    });
+  }
+
+  changeOrder(order, dir) {
+    this.setState({
+      orderBy: order,
+      orderDir: dir
     });
   }
 
@@ -45,6 +55,12 @@ class App extends Component {
     this.setState({
       myAppointments: tempApts
     });
+  }
+
+  searchApts(query) {
+    this.setState({
+      queryText: query
+    })
   }
 
   componentDidMount() {
@@ -71,16 +87,30 @@ class App extends Component {
       order = -1;
     }
 
-    filteredApts.sort((a, b) => {
-      if (
-        a[this.state.orderBy].toLowerCase() <
-        b[this.state.orderBy].toLowerCase()
-      ) {
-        return -1 * order;
-      } else {
-        return 1 * order;
-      }
-    });
+    filteredApts = filteredApts
+      .sort((a, b) => {
+        if (
+          a[this.state.orderBy].toLowerCase() <
+          b[this.state.orderBy].toLowerCase()
+        ) {
+          return -1 * order;
+        } else {
+          return 1 * order;
+        }
+      })
+      .filter(eachItem => {
+        return (
+          eachItem['petName']
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase()) ||
+          eachItem['ownerName']
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase()) ||
+          eachItem['aptNotes']
+            .toLowerCase()
+            .includes(this.state.queryText.toLowerCase())
+        );
+      });
 
     return (
       <main className="page bg-white" id="petratings">
@@ -96,6 +126,8 @@ class App extends Component {
                 <SearchAppointments
                   orderBy={this.state.orderBy}
                   orderDir={this.state.orderDir}
+                  changeOrder={this.changeOrder}
+                  searchApts={this.state.searchApts}
                 />
                 <ListAppointments
                   appointments={filteredApts}
